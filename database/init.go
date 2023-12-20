c9323d9ff75d6b1a447f60326e688c25f8482159
B2N10X30 <samuelbonux10@gmail.com>
2023-12-20 22:12:58 +0100

    sqling

--- database/init.go
+++ database/init.go
@@ -19,6 +19,8 @@ var DB *sql.DB
 
 // init the dB and try connecting
 func InitDB() error {
+	//constring for postgreSQL
+	//host=your_host port=your_port user=your_user password=your_password dbname=your_dbname sslmode=disable
 	dataSrcName := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("PG_HOST"), os.Getenv("PG_PORT"), os.Getenv("PG_USER"),
 		os.Getenv("PG_PASSWORD"), os.Getenv("PG_DBNAME"))
